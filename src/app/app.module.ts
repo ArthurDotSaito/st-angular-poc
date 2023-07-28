2d722d3b5844269a36ee48042c9d1decd49dc9ee
Arthur Saito <arthur_saito1998@hotmail.com>
2023-07-28 00:43:11 -0300

    chore: add main components to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardBoxComponent } from './components/card-box/card-box.component';
+import { CarouselItemsComponent } from './components/carousel-items/carousel-items.component';
+import { AmountBoxComponent } from './components/amount-box/amount-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    HomeComponent,
+    HeaderComponent,
+    CardBoxComponent,
+    CarouselItemsComponent,
+    AmountBoxComponent
   ],
   imports: [
     BrowserModule,
