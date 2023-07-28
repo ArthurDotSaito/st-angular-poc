589220d5b182f96aac4e9ccbd058464c026df964
Arthur Saito <arthur_saito1998@hotmail.com>
2023-07-28 01:13:05 -0300

    chore: importing all svgIconsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/card/card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CardBoxComponent } from './components/card-box/card-box.component';
 import { CarouselItemsComponent } from './components/carousel-items/carousel-items.component';
 import { AmountBoxComponent } from './components/amount-box/amount-box.component';
+import { SvgComponentsModule } from 'src/assets/svgs/svg.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
     HomeComponent,
     HeaderComponent,
     CardBoxComponent,
     CarouselItemsComponent,
-    AmountBoxComponent
+    AmountBoxComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, SvgComponentsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
