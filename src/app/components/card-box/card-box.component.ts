b26276c1f5fe966c56dd9926851429f44d7f113c
Arthur Saito <arthur_saito1998@hotmail.com>
2023-07-30 20:22:10 -0300

    chore: init cardBoxComponent

--- src/app/components/card-box/card-box.component.ts
+++ src/app/components/card-box/card-box.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AccountDataModel } from 'src/app/model/account-data-model';
 
 @Component({
   selector: 'app-card-box',
   templateUrl: './card-box.component.html',
-  styleUrls: ['./card-box.component.css']
+  styleUrls: ['./card-box.component.css'],
 })
 export class CardBoxComponent {
-
+  accountData: AccountDataModel = {
+    name: '',
+    account: {
+      agency: '0001',
+      number: '00.000000-0',
+    },
+    card: {
+      limit: 0,
+      number: '0000',
+    },
+  };
 }
